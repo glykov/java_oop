2c9119807c4e92eb8d54f4a2f6cb019fcbe84fb2
Gleb Lykov <glebdom@gmail.com>
2022-11-21 19:52:35 +0300

    Домашнее задание к семинару 7

---
+++ lesson_07/echo_client.py
@@ -0,0 +1,22 @@
+import sys
+from socket import *
+
+server_host = 'localhost'
+server_port = 8189
+
+message = [b'Hello network world']
+
+if len(sys.argv) > 1:
+    server_host = sys.argv[1]
+    if len(sys.argv) > 2:
+        message = (x.encode() for x in sys.argv[2:])
+
+sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj.connect((server_host, server_port))
+
+for line in message:
+    sockobj.send(line)
+    data = sockobj.recv(1024)
+    print('Client received:', data)
+
+sockobj.close()
